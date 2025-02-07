1728804a74e004454895a16990f9aaae15898eef
Coriolan Popescu <51032215+pcoriola@users.noreply.github.com>
2025-02-07 15:26:55 +0200

    Update main_using_managed_identity.tf

--- main_using_managed_identity.tf
+++ main_using_managed_identity.tf
@@ -1,33 +1,33 @@
-#Extract subscription details
-data "azurerm_subscription" "current" {}
-
-#Extract RG name
+#Extract resource group details
 data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
-#Extract Key Vault details
-data "azurerm_key_vault" "keyvault" {
-  name                = var.key_vault_name
+#Extract storage account details
+data "azurerm_storage_account" "storage_account" {
+  name                = var.storage_account_name
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
+#Extract workspace details
+data "azurerm_machine_learning_workspace" "workspace" {
+  for_each            = toset(var.workspace_name)
+  name                = each.key
   resource_group_name = var.resource_group_name
 }
 
-#Extract storage account name
-data "azurerm_storage_account" "storage_account" {
-  name                    = var.storage_account
-  resource_group_name     = data.azurerm_resource_group.resource_group.name
-}
 #Create storage containers
 resource "azurerm_storage_container" "storage_container" {
-  count                 = length(var.workspace_id)
-  name                  = "storage-container-${var.workspace_id[count.index]}"
-  storage_account_name  = azurerm_storage_account.storage_account.name
+  count                = length(var.workspace_names)
+  name                 = "storage-container-${var.workspace_names[count.index]}"
+  storage_account_name = data.azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
 
+#Enable system-assigned managed identity on workspaces
 resource "azurerm_machine_learning_workspace" "workspace" {
-  count               = length(var.workspace_id)
-  name                = "workspace-${var.workspace_id[count.index]}"
+  count               = length(var.workspace_names)
+  name                = var.workspace_names[count.index]
   resource_group_name = var.resource_group_name
   location            = var.location
 
@@ -36,10 +36,10 @@ resource "azurerm_machine_learning_workspace" "workspace" {
   }
 }
 
-#Role assignmanet to grant access for MI to storage container
+#Perform role assignment to allow each ml workspace to access only its storage container
 resource "azurerm_role_assignment" "container_access" {
-  for_each            = toset(var.workspace_id)
-  scope               = azurerm_storage_container.storage_container[each.key].resource_manager_id
+  count               = length(var.workspace_names)
+  scope               = azurerm_storage_container.storage_container[count.index].resource_manager_id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id        = azurerm_machine_learning_workspace.workspace[each.key].identity[0].principal_id
+  principal_id        = data.azurerm_machine_learning_workspace.workspace[var.workspace_names[count.index]].identity[0].principal_id
 }
