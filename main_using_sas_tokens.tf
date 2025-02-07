012da6b0b6e470580eb416c8a027b6e1151ce4d4
Coriolan Popescu <51032215+pcoriola@users.noreply.github.com>
2025-02-07 16:05:02 +0200

    Update main_using_sas_tokens.tf

--- main_using_sas_tokens.tf
+++ main_using_sas_tokens.tf
@@ -1,49 +1,36 @@
 #Extract subscription details
 data "azurerm_subscription" "current" {}
 
-#Extract RG name
+#Extract resource group details
 data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
-#Extract Key Vault details
-data "azurerm_key_vault" "keyvault" {
-  name                = var.key_vault_name
+#Extract storage account details
+data "azurerm_storage_account" "storage_account" {
+  name                = var.storage_account_name
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
+#Extract existing ml workspaces
+data "azurerm_machine_learning_workspace" "workspace" {
+  for_each            = toset(var.workspace_list)
+  name                = each.key
   resource_group_name = var.resource_group_name
 }
 
-#Extract storage account name
-data "azurerm_storage_account" "storage_account" {
-  name                    = var.storage_account
-  resource_group_name     = data.azurerm_resource_group.resource_group.name
-}
-#Create storage containers
+# Create storage containers for each ML workspace
 resource "azurerm_storage_container" "storage_container" {
-  count                 = length(var.workspace_id)
-  name                  = "storage-container-${var.workspace_id[count.index]}"
-  storage_account_name  = azurerm_storage_account.storage_account.name
+  count                = length(var.workspace_list)
+  name                 = "storage-container-${var.workspace_list[count.index]}"
+  storage_account_name = data.azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
 
-(* #Create access policy for workspaces
-resource "null_resource" "set_acl" {
-  provisioner "local-exec" {
-    command = <<EOT
-      az storage container policy create \
-        --account-name ${azurerm_storage_account.storage_account.name} \
-        --container-name ${azurerm_storage_container.storage_container.name} \
-        --name "write-access-policy" \
-        --start $(date -u -d "now" +%Y-%m-%dT%H:%MZ) \
-        --expiry $(date -u -d "1 hour" +%Y-%m-%dT%H:%MZ) \
-        --permissions rl
-    EOT
-  }
-} *)
-
 # Generate SAS Token for each workspace
 resource "azurerm_storage_blob_container_sas" "sas_token" {
-  count                = length(var.workspace_id)
-  storage_account_name = azurerm_storage_account.storage_account.name
+  count                = length(var.workspace_list)
+  storage_account_name = data.azurerm_storage_account.storage_account.name
   container_name       = azurerm_storage_container.storage_container[count.index].name
   https_only           = true
 
@@ -60,15 +47,15 @@ resource "azurerm_storage_blob_container_sas" "sas_token" {
   }
 }
 
-# Outputs - Securely Retrieve SAS Tokens for ML Workspaces
-output "sas_token" {
+#Output SAS tokens
+output "sas_tokens" {
   value = { for idx, token in azurerm_storage_blob_container_sas.sas_token :
-    azurerm_storage_container.storage_container[idx].name => sas_token.sas
+    azurerm_storage_container.storage_container[idx].name => token.sas
   }
   sensitive = true
 }
 
-#Create Key Vault
+#Create Key Vault to store SAS tokens
 resource "azurerm_key_vault" "key_vault" {
   name                = "keyvault-${data.azurerm_storage_account.storage_account.name}"
   resource_group_name = data.azurerm_resource_group.resource_group.name
@@ -77,20 +64,19 @@ resource "azurerm_key_vault" "key_vault" {
   tenant_id           = data.azurerm_subscription.current.tenant_id
 }
 
-#Upload sas token into KV
+#Upload SAS token into key vault
 resource "azurerm_key_vault_secret" "sas_token_kv" {
-  count        = length(var.workspace_id)
-  name         = "sas-token-kv-${var.workspace_id[count.index]}"
+  count        = length(var.workspace_names)
+  name         = "sas-token-${var.workspace_list[count.index]}"
   value        = azurerm_storage_blob_container_sas.sas_token[count.index].sas
-  key_vault_id = azurerm_key_vault.ml_key_vault.id
+  key_vault_id = azurerm_key_vault.key_vault.id
 }
 
-# Allow ml workspace access to SAS token
+# Allow access to for each workspace to own SAS token
 resource "azurerm_key_vault_access_policy" "workspace_access" {
-  count        = length(var.workspace_id)
-  key_vault_id = azurerm_key_vault.keyvault.id
-  tenant_id = data.azurerm_subscription.current.tenant_id
-  object_id = var.workspace_id[count.index]
+  count        = length(var.workspace_list)
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_subscription.current.tenant_id
+  object_id    = data.azurerm_machine_learning_workspace.workspace[var.workspace_list[count.index]].identity[0].principal_id
   secret_permissions = ["Get"]
 }
-
