08d4d20db6c70c88ccd3422f955df8fe347ef576
Coriolan Popescu <51032215+pcoriola@users.noreply.github.com>
2025-02-07 15:52:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,42 @@
+#Resource group name
 variable "resource_group_name" {
   description = "Resource Group"
   type        = string
 }
 
-variable "storage_account_name" {
-  description = "Storage Account"
-  type        = string
-}
-
+#Location
 variable "location" {
   description = "Region"
   type        = string
   default     = "West Europe"
 }
 
-variable "workspace_id" {
-  description = "ML Project Workspace IDs"
+#Storage Account Name
+variable "storage_account_name" {
+  description = "Storage Account"
+  type        = string
+}
+
+#Key Vault
+variable "key_vault_name" {
+  description = "Key Vault for storing SAS tokens"
+  type        = string
+}
+
+#List of ML workspaces
+variable "workspace_list" {
+  description = "List of ML workspaces"
   type        = list(string)
 }
+
+#Tenant_id
+variable "tenant_id" {
+  description = "Tenant ID"
+  type        = string
+}
+
+#Workspace ID
+#variable "workspace_id" {
+#  description = "ML Project Workspace IDs"
+#  type        = list(string)
+#}
